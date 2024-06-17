5b8d7e62a222f56311a2265406dbd3d03eff4067
Bruno Santos <brsantos197@gmail.com>
2024-06-16 23:59:31 -0300

    first commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rocketseat-hmg"
+  location = "brazilsouth"
+  tags = {
+    Iac = true
+  }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "rocketseat-vnet"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
+  tags = {
+    Iac = true
+  }
+}
